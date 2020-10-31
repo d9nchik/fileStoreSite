bf9982774360e1531c3e8e75dbc4dbed297838ae
d9nich <dan.galaiko@gmail.com>
2020-10-31 18:48:10 +0200

    Provided date to files

--- myfiles/models.py
+++ myfiles/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class File(models.Model):
     name = models.CharField(max_length=64, null=True)
     path = models.FileField(upload_to='files')
     unique = models.CharField(max_length=5)
+    date_of_upload = models.DateTimeField(default=now)
